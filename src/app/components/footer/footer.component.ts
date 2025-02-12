07e720dfe723c88699401d31b26f4e6106844225
Edgar <eath2497@gmail.com>
2025-02-11 23:23:25 -0500

    estilos y textos en idiomas

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  email: string = 'eath2497@gmail.com';
+  designed: string = 'eath2497@gmail.com';
+
+  constructor(
+    private translate: TranslateService
+  ){
+    this.translate.setDefaultLang('es');
+  }
+}
